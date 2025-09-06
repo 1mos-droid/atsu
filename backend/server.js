d93c6c6abc136512e4e7f8bd9fff22e93e14454f
1mos-droid <isemuk8@gmail.com>
2025-09-06 17:11:48 +0000

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 // Middleware
 app.use(cors());
@@ -123,5 +124,6 @@ app.use((req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
+
