c1931d701524d9324738b446cdcdf8d8765ca3fe
1mos-droid <isemuk8@gmail.com>
2025-09-07 10:31:21 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -64,7 +63,7 @@ app.post("/api/agents", (req, res) => {
     phone,
     role,
     department,
-    status
+    status,
   };
 
   agents.push(newAgent);
@@ -92,7 +91,7 @@ app.put("/api/agents/:id", (req, res) => {
     phone,
     role,
     department,
-    status
+    status,
   };
 
   writeJsonData(agents);
@@ -118,7 +117,7 @@ app.delete("/api/agents/:id", (req, res) => {
 // ========================
 // Catch-all: serve frontend index.html
 // ========================
-app.use((req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend", "index.html"));
 });
 
